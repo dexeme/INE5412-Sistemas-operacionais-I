c8cbcb46adc8aca12881a741eb083d37053627e9
dexeme <dexe.me12@gmail.com>
2023-08-28 11:24:15 -0300

    Adicionado mais parâmetros para os processos

--- Trabalho1/process.h
+++ Trabalho1/process.h
@@ -8,20 +8,36 @@
 class ProcessParams
 {
 public:
-	ProcessParams(int c, int d, int p);
+	ProcessParams(int c, int d, int p, pid_t pid);
+
+    // Get pid
+    pid_t getPid();
 
 	// Get creation time
     int getCreationTime();
 
+    // Get running time
+    int getRunningTime();
+
+    // Get finish time
+    int getFinishTime();
+
 	// Get duration
     int getDuration();
+
+    // Get state
+    int getState();
 
 	// Get priority
 	int getPriority();
 
 private:
+    pid_t pid;
     int creation_time;
+    int running_time;
+    int finish_time;
     int duration;
+    int state;
     int priority;
 };
 
