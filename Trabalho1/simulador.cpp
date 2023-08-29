987770f5a4460bdc4658fa37e785d9e853718962
dexeme <dexe.me12@gmail.com>
2023-08-29 18:01:24 -0300

    Adicionando simulador cpp

---
+++ Trabalho1/simulador.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "file.h"
+#include "process.h"
+#include "scheduler.h"
+
+
+using namespace std;
+
+Simulador::Simulador() {}
+
+Simulador::~Simulador() {}
+
+void Simulador::start_simulator() {}
