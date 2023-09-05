2b1e3d43f0ab99f465db644e009ca06ac89f105b
dexeme <dexe.me12@gmail.com>
2023-09-05 18:09:34 -0300

    Parando de usar ponteiro e criando objeto

--- Trabalho1/kernel.cpp
+++ Trabalho1/kernel.cpp
@@ -21,30 +21,34 @@ using namespace std;
 
 void Kernel::start(string scheduler_type) {
     cout << "DEBUG: Kernel iniciado" << endl;
-    vector<Process*> processos;
+    vector<Process> processos;
     File file;
     vector<ProcessParams> processes_lidos = file.read_file();
     for (unsigned int i = 0; i < processes_lidos.size(); i++) {
         ProcessParams p = processes_lidos[i];
-        Process* processo_novo = create_process(p);
+        Process processo_novo = create_process(p);
         processos.push_back(processo_novo);
         cout << "DEBUG: Processo " << p.get_pid() << " criado" << endl;
+        cout << "DEBUG: Tempo de duracao do processo " << p.get_pid() << ": " << p.get_duration() << endl;
     }
+
+    Scheduler* scheduler = nullptr;
 
     if (scheduler_type == "FCFS") {
         cout << "DEBUG: Iniciando FCFS" << endl;
-        ARM cpu;
-        FCFS scheduler(cpu);
+        ARM cpu = ARM();
+        scheduler = new FCFS(cpu);
+
         
     } else if (scheduler_type == "SJF") {
         cout << "DEBUG: Iniciando SJF" << endl;
-        ARM arm(cpu);
-        SJF scheduler(cpu);
-
+        ARM cpu = ARM();
+        scheduler = new SJF(cpu);
 
         for (unsigned int i = 0; i < processos.size(); i++) {
-            cout << "DEBUG: Processo " << processos[i]->getPid() << " adicionado ao escalonador" << endl;
-            scheduler.add_process(*processos[i]);
+            cout << "DEBUG: Processo " << processos[i].getPid() << " adicionado ao escalonador" << endl;
+            scheduler->add_process(processos[i]);
+            scheduler->organize_ready_queue();
         }
 
 
@@ -54,13 +58,13 @@ void Kernel::start(string scheduler_type) {
     }
 }
 
-Process* Kernel::create_process(ProcessParams params) {
+Process Kernel::create_process(ProcessParams params) {
     int creation_data = params.get_creation_data();
     int priority = params.get_priority();
     int duration = params.get_duration();
     int pid = params.get_pid();
 
-    Process* processo_novo = new Process(creation_data, priority, duration, pid);
+    Process processo_novo = Process(creation_data, duration, priority, pid);
 
     cout << "DEBUG: Criando processo " << pid << endl;
 
