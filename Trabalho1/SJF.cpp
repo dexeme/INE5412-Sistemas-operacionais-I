54df65f18e7fd0e1d0d5c60c3e06a5788fbb4b12
dexeme <dexe.me12@gmail.com>
2023-09-05 18:10:44 -0300

    Método do algoritmo SJF organiza a fila de prontos

--- Trabalho1/SJF.cpp
+++ Trabalho1/SJF.cpp
@@ -14,19 +14,44 @@ public:
     ~SJF() {}
 
     void execute() {}
-
-    void organize_ready_queue() {
-        vector<Process> processos;
-        while (!is_ready_queue_empty()) {
-            processos.push_back(get_next_process());
-        }
-        sort(processos.begin(), processos.end(), [](Process a, Process b) {
-            return a.getDuration() < b.getDuration();
-        });
-        for (Process processo : processos) {
-            add_process(processo);
+    // 2 4 3 1
+void organize_ready_queue() {
+    vector<Process> processos;
+    
+    cout << "Antes da organização: [";
+    while (!is_ready_queue_empty()) {
+        Process processo = get_next_process();
+        processos.push_back(processo);
+        cout << processo.getPid() << " (" << processo.getDuration() << ")";
+        if (!is_ready_queue_empty()) {
+            cout << ", ";
         }
     }
+    cout << "]" << endl;
+
+    sort(processos.begin(), processos.end(), [](Process& a, Process& b) {
+        return a.getDuration() < b.getDuration();
+    });
+
+    cout << "Após a organização: [";
+    for (size_t i = 0; i < processos.size(); i++) {
+        cout << processos[i].getPid() << " (" << processos[i].getDuration() << ")";
+        if (i != processos.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+
+    // Adicione os processos organizados de volta à fila de prontos
+    for (const Process& processo : processos) {
+        add_process(processo);
+    }
+
+    cout << "Fila de prontos organizada com sucesso!" << endl;
+}
+
+
+
 
 
 
