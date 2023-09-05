ae70178b031bb90462179e764838de510c24dcf7
dexeme <dexe.me12@gmail.com>
2023-09-05 18:11:24 -0300

    Arrumando formatação

--- Trabalho1/SJF.cpp
+++ Trabalho1/SJF.cpp
@@ -15,44 +15,38 @@ public:
 
     void execute() {}
     // 2 4 3 1
-void organize_ready_queue() {
-    vector<Process> processos;
-    
-    cout << "Antes da organização: [";
-    while (!is_ready_queue_empty()) {
-        Process processo = get_next_process();
-        processos.push_back(processo);
-        cout << processo.getPid() << " (" << processo.getDuration() << ")";
-        if (!is_ready_queue_empty()) {
-            cout << ", ";
+    void organize_ready_queue() {
+        vector<Process> processos;
+        
+        cout << "Antes da organização: [";
+        while (!is_ready_queue_empty()) {
+            Process processo = get_next_process();
+            processos.push_back(processo);
+            cout << processo.getPid() << " (" << processo.getDuration() << ")";
+            if (!is_ready_queue_empty()) {
+                cout << ", ";
+            }
         }
-    }
-    cout << "]" << endl;
+        cout << "]" << endl;
 
-    sort(processos.begin(), processos.end(), [](Process& a, Process& b) {
-        return a.getDuration() < b.getDuration();
-    });
+        sort(processos.begin(), processos.end(), [](Process& a, Process& b) {
+            return a.getDuration() < b.getDuration();
+        });
 
-    cout << "Após a organização: [";
-    for (size_t i = 0; i < processos.size(); i++) {
-        cout << processos[i].getPid() << " (" << processos[i].getDuration() << ")";
-        if (i != processos.size() - 1) {
-            cout << ", ";
+        cout << "Após a organização: [";
+        for (size_t i = 0; i < processos.size(); i++) {
+            cout << processos[i].getPid() << " (" << processos[i].getDuration() << ")";
+            if (i != processos.size() - 1) {
+                cout << ", ";
+            }
         }
+        cout << "]" << endl;
+
+        // Adicione os processos organizados de volta à fila de prontos
+        for (const Process& processo : processos) {
+            add_process(processo);
+        }
+
+        cout << "Fila de prontos organizada com sucesso!" << endl;
     }
-    cout << "]" << endl;
-
-    // Adicione os processos organizados de volta à fila de prontos
-    for (const Process& processo : processos) {
-        add_process(processo);
-    }
-
-    cout << "Fila de prontos organizada com sucesso!" << endl;
-}
-
-
-
-
-
-
 };
