e528c3d520b1f46ba69cccb50d4223839369fb3d
dexeme <dexe.me12@gmail.com>
2023-09-10 20:09:36 -0300

    Ajuste no FCFS

--- Trabalho1/FCFS.cpp
+++ Trabalho1/FCFS.cpp
@@ -14,32 +14,78 @@ public:
 
     ~FCFS() {}
 
-    bool execute() {
-        printa_fila_de_prontos(); // DEBUG
-        Process* processo_atual = get_current_process();
-        CPU &cpu = get_cpu();
-        queue<Process>& fila_de_prontos = get_ready_queue();
+bool execute() {
+    printa_fila_de_prontos(); // DEBUG
+    Process* processo_atual = get_current_process();
+    CPU &cpu = get_cpu();
+    queue<Process>& fila_de_prontos = get_ready_queue();
+    queue<Process>& fila_de_executando = get_running_queue();
 
-        if (processo_atual == nullptr) { // Se o processo atual é nulo
-            if (is_ready_queue_empty()) {
+    if (!processo_atual) { // Se o processo atual é nulo
+        if (fila_de_executando.empty()) {
+            cout << "NULLPTR: Processo atual é nulo" << endl;
+            if (fila_de_prontos.empty()) {
                 return false;
             }
+            cout << "DEBUG: Processo atual selecionado: " << fila_de_prontos.front().getPid() << endl;
+            cout << "DEBUG: Fila de prontos não está vazia! Próximo processo: " << fila_de_prontos.front().getPid() << " | TR: " << fila_de_prontos.front().getRemainingTime() << "/" << fila_de_prontos.front().getDuration() << endl;
+            int prioridade_atual = fila_de_executando.front().getPriority();
+
             Process& proximo_processo = get_next_process();
+            fila_de_executando.push(proximo_processo);
+            fila_de_prontos.pop();
+
+            printa_fila_de_prontos();
+
             cpu.run_process(proximo_processo, cpu); // Executa o próximo processo na CPU
+
+            // Atualize o tempo restante do processo atual
+            fila_de_executando.front().setRemainingTime(proximo_processo.getRemainingTime());
+
             if (proximo_processo.getRemainingTime() == 0) {
                 finish_process(proximo_processo); // Se o processo terminou, finaliza ele
             }
-            return !is_ready_queue_empty(); // Retorna se a fila de prontos está vazia, para saber se o escalonador terminou
+            return !fila_de_prontos.empty(); // Retorna se a fila de prontos está vazia, para saber se o escalonador terminou
         }
-        if (cpu.is_idle() && !processo_atual->is_finished()) { // Se a CPU está livre e o processo atual não está finalizado
-            if (!fila_de_prontos.empty()) {
-                Process processo_atual = fila_de_prontos.front();
-                fila_de_prontos.pop();
-                cpu.run_process(processo_atual, cpu);
+
+        // Parte da preempção
+        if (!fila_de_prontos.empty()) {
+            cout << "DEBUG: Checando se há processos de maior prioridade" << endl;
+            Process& proximo_processo = fila_de_prontos.front();
+            int prioridade_processo_atual = fila_de_executando.front().getPriority();
+            int prioridade_proximo_processo = proximo_processo.getPriority();
+            cout << "DEBUG: Prioridade do processo atual: " << prioridade_processo_atual << endl;
+            cout << "DEBUG: Prioridade do próximo processo: " << prioridade_proximo_processo << endl;
+            if (prioridade_proximo_processo > prioridade_processo_atual) {
+                cout << "DEBUG: Processo atual é preemptado" << endl; // ISSO
+            }
+            int tempo_restante_do_processo_apos_execucao = cpu.run_process(fila_de_executando.front(), cpu);
+
+            // Atualize o tempo restante do processo atual
+            fila_de_executando.front().setRemainingTime(tempo_restante_do_processo_apos_execucao);
+
+            if (tempo_restante_do_processo_apos_execucao == 0) {
+                finish_process(fila_de_executando.front());
+            }
+            return !fila_de_prontos.empty();
+        } else {
+            // Se não houver preempção, execute apenas o processo atual
+            int tempo_restante_do_processo_apos_execucao = cpu.run_process(fila_de_executando.front(), cpu);
+
+            // Atualize o tempo restante do processo atual
+            fila_de_executando.front().setRemainingTime(tempo_restante_do_processo_apos_execucao);
+
+            if (tempo_restante_do_processo_apos_execucao == 0) {
+                finish_process(fila_de_executando.front());
             }
         }
+
         return false;
     }
+
+    // Restante do código para o caso em que há um processo em execução
+}
+
 
     bool check_preemption(Process processo_atual) { return false; } // FCFS não tem preempção
 
