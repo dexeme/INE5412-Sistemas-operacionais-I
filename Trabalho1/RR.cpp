38463f6948734e972fbadeaf6ce771d374c44251
dexeme <dexe.me12@gmail.com>
2023-09-14 11:27:04 -0300

    TODOS ALGORITMOS FORAM IMPLEMENTADOS PERFEITAMENTE UIHUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- Trabalho1/RR.cpp
+++ Trabalho1/RR.cpp
@@ -13,24 +13,23 @@ public:
     ~RR() {}
 
 bool execute() {
-
     clear_finished_queue();
     CPU &cpu = get_cpu();
-    queue<Process>& fila_de_prontos = get_ready_queue();
-    queue<Process>& fila_de_executando = get_running_queue();
+    queue<Process> &fila_de_prontos = get_ready_queue();
+    queue<Process> &fila_de_executando = get_running_queue();
 
-    if (fila_de_executando.empty()) { // Se o processo atual é nulo
+    if (fila_de_executando.empty()) {
         if (is_ready_queue_empty()) {
-            cout << "Fila de prontos vazia, executando ultimo processo" << endl;
         }
     }
-    if (is_running_queue_empty()) { // Se o processo atual é nulo
+
+    if (is_running_queue_empty()) {
         if (is_ready_queue_empty()) {
         }
 
-        Process& proximo_processo = get_next_process();
-        queue<Process>& running_queue = get_running_queue();
-        queue<Process>& ready_queue = get_ready_queue();
+        Process &proximo_processo = get_next_process();
+        queue<Process> &running_queue = get_running_queue();
+        queue<Process> &ready_queue = get_ready_queue();
 
         running_queue.push(proximo_processo);
         ready_queue.pop();
@@ -38,50 +37,50 @@ bool execute() {
         set_running_queue(running_queue);
         set_ready_queue(ready_queue);
 
-
         auto [remaining_time, quantum_time] = cpu.run_process(fila_de_executando.front(), cpu); // Executa o próximo processo na CPU
         
-        // Atualize o tempo restante do processo atual
+        // Atualize o tempo restante do processo atual e o quantum
         fila_de_executando.front().setRemainingTime(remaining_time);
         fila_de_executando.front().setCurrentQuantum(quantum_time);
         
         if (remaining_time == 0) {
             finish_process(fila_de_executando.front()); // Se o processo terminou, finaliza ele
         }
-        return !is_ready_queue_empty(); // Retorna se a fila de prontos está vazia, para saber se o escalonador terminou
+        
+        return true; // Continue a execução do escalonador
     }
     
     // Parte da preempção
     if (!is_ready_queue_empty()) {
-        Process& proximo_processo = fila_de_prontos.front();
+        Process &proximo_processo = fila_de_prontos.front();
         int quantum_processo_atual = fila_de_executando.front().getCurrentQuantum();
 
-        if (quantum_processo_atual == 2) {
+        if (quantum_processo_atual == 2) { // Se o quantum expirou
             switch_process(fila_de_executando.front(), fila_de_prontos.front());
 
-            // seta o quantum do processo atual para 0
+            // Zera o quantum do processo atual
             fila_de_executando.front().setCurrentQuantum(0);
 
-            // remove o processo atual da fila de executando e coloca ele na fila de prontos
-                        
+            // Remove o processo atual da fila de execução e coloca-o na fila de prontos
             fila_de_prontos.push(fila_de_executando.front());
             fila_de_executando.push(proximo_processo);
             fila_de_prontos.pop();
             fila_de_executando.pop();
             set_running_queue(fila_de_executando);
             set_ready_queue(fila_de_prontos);
-            }
-            auto [remaining_time, quantum_time] = cpu.run_process(fila_de_executando.front(), cpu); // Executa o próximo processo na CPU
-            
-            // Atualize o tempo restante do processo atual
-            fila_de_executando.front().setRemainingTime(remaining_time);
-            fila_de_executando.front().setCurrentQuantum(quantum_time);
-            
-            if (remaining_time == 0) {
-                finish_process(fila_de_executando.front()); // Se o processo terminou, finaliza ele
-            }
+        }
 
-            return !is_ready_queue_empty();
+        auto [remaining_time, quantum_time] = cpu.run_process(fila_de_executando.front(), cpu); // Executa o próximo processo na CPU
+        
+        // Atualize o tempo restante do processo atual e o quantum
+        fila_de_executando.front().setRemainingTime(remaining_time);
+        fila_de_executando.front().setCurrentQuantum(quantum_time);
+        
+        if (remaining_time == 0) {
+            finish_process(fila_de_executando.front()); // Se o processo terminou, finaliza ele
+        }
+
+        return true; // Continue a execução do escalonador
     } else {
         // Se não houver preempção, execute apenas o processo atual
         auto [remaining_time, quantum_time] = cpu.run_process(fila_de_executando.front(), cpu);
@@ -96,6 +95,7 @@ bool execute() {
     
     return false;
 }
+
 
     
 
